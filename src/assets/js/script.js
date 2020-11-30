6562aecabcb01bc4af4475ab2ebfa2f1bb75d43a
Luka <lzunic99@gmail.com>
2020-11-30 10:47:12 +0100

    Added functionality for API data fetching

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,18 +1,19 @@
+import './config.js'
+
 
 let city = document.getElementById('city');
-city.addEventListener("keyup", (event) => {
-    if(event.key === 'Enter'){
-        event.preventDefault();
-        cityName = city.value;
-    }
+city.addEventListener('keyup', (event)=>{
+    if(event.key == 'Enter'){
+        $.getJSON(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&APPID=${config.APIkey}` 
+            ,(data)=>{
+                console.log(data);
+                document.getElementById("test").innerHTML = data.coord.lat;
+            }
+        );
+        
+    } 
 })
-
-$.getJSON(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${config.APIkey}`,
-    function(data){
-        console.log(data);
-    }
-);
 
 
 
